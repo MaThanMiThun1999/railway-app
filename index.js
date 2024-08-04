e6c28e245db237b09af8ee03a51cd13d869481e3
MaThanMiThun1999 <mathanmithun8838@gmail.com>
2024-08-04 23:33:26 +0530

    ADD EXPRESS SERVER WITH SCRAPER

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
+const puppeteer = require("puppeteer");
 const express = require("express");
 require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => res.send(`<h1>RailWaY app Running on port ${PORT}!</h1>`));
+const checkScrap = async () => {
+  const browser = await puppeteer.launch({
+    headless: true,
+    slowMo: 100,
+  });
+  console.log(`Browser launched...!`);
+  const page = await browser.newPage();
+  await page.goto("https://example.com");
+  console.log(`Navigated to ${page.url()}. Title: ${await page.title()}. Current URL: ${await page.url()}`);
+  const title = await page.title();
+  console.log(`Title: ${title}`);
+  console.log("Browser Closing");
+  await browser.close();
+  console.log(`Browser closed`);
+};
+
+app.get("/", (req, res) => {
+    res.send(`<h1>RailWaY app Running on port ${PORT}!</h1>`)
+});
 
 app.get("/send", (req, res) => {
+  checkScrap();
   res.send(`<h1>Successfully Email Sended</h1>`);
 });
 
