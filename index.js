67cae42819b4ecf7094954654de5ba791d7a3581
MaThanMiThun1999 <mathanmithun8838@gmail.com>
2024-08-04 23:37:57 +0530

    "Added browser launch arguments and updated index route response in index.js"

--- index.js
+++ index.js
@@ -7,6 +7,17 @@ const PORT = process.env.PORT || 5000;
 
 const checkScrap = async () => {
   const browser = await puppeteer.launch({
+    args: [
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+      "--disable-dev-shm-usage",
+      "--disable-accelerated-2d-canvas",
+      "--no-first-run",
+      "--no-zygote",
+      "--single-process",
+      "--disable-gpu",
+      "--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36",
+    ],
     headless: true,
     slowMo: 100,
   });
@@ -22,7 +33,7 @@ const checkScrap = async () => {
 };
 
 app.get("/", (req, res) => {
-    res.send(`<h1>RailWaY app Running on port ${PORT}!</h1>`)
+  res.send(`<h1>RailWaY app Running on port ${PORT}!</h1>`);
 });
 
 app.get("/send", (req, res) => {
