0510888dde1527e633b530267159ea3e757055bb
MaThanMiThun1999 <mathanmithun8838@gmail.com>
2024-08-04 23:49:28 +0530

    "Refactored checkScrap function to add error handling and viewport settings"

--- index.js
+++ index.js
@@ -6,30 +6,47 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const checkScrap = async () => {
-  const browser = await puppeteer.launch({
-    args: [
-      "--no-sandbox",
-      "--disable-setuid-sandbox",
-      "--disable-dev-shm-usage",
-      "--disable-accelerated-2d-canvas",
-      "--no-first-run",
-      "--no-zygote",
-      "--single-process",
-      "--disable-gpu",
-      "--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36",
-    ],
-    headless: true,
-    slowMo: 100,
-  });
-  console.log(`Browser launched...!`);
-  const page = await browser.newPage();
-  await page.goto("https://example.com");
-  console.log(`Navigated to ${page.url()}. Title: ${await page.title()}. Current URL: ${await page.url()}`);
-  const title = await page.title();
-  console.log(`Title: ${title}`);
-  console.log("Browser Closing");
-  await browser.close();
-  console.log(`Browser closed`);
+  let browser;
+  try {
+    browser = await puppeteer.launch({
+      args: [
+        "--no-sandbox",
+        "--disable-setuid-sandbox",
+        "--disable-dev-shm-usage",
+        "--disable-accelerated-2d-canvas",
+        "--no-first-run",
+        "--no-zygote",
+        "--single-process",
+        "--disable-gpu",
+        "--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36",
+      ],
+      headless: true,
+      slowMo: 100,
+    });
+    console.log(`Browser launched...!`);
+
+    const page = await browser.newPage();
+    await page.setViewport({ width: 1280, height: 800 });
+    await page.evaluateOnNewDocument(() => {
+      Object.defineProperty(navigator, "webdriver", {
+        get: () => false,
+      });
+    });
+    await page.goto("https://example.com");
+
+    console.log(`Navigated to ${page.url()}. Title: ${await page.title()}. Current URL: ${await page.url()}`);
+    const title = await page.title();
+    console.log(`Title: ${title}`);
+
+    console.log("Browser Closing");
+  } catch (error) {
+    console.log(`Error occured while creating the browser instance => ${error}`);
+  } finally {
+    if (browser) {
+      await browser.close();
+      console.log("Browser Closed");
+    }
+  }
 };
 
 app.get("/", (req, res) => {
